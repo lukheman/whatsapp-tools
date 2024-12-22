8a25121c0b70e6207705e78daa94a3a33e89815a
Akmal <akmalthemalong@gmail.com>
2024-12-22 17:35:19 +0800

    create: components.js

---
+++ src/ui/components.js
@@ -0,0 +1,20 @@
+function banner() {
+  console.log(`
+|  \\/  | ___ _ __ | |_ __ \\ \\      / /_ _
+| |\\/| |/ _ \\ '_ \\| __/ _\` \\ \\ /\\ / / _\` |
+| |  | |  __/ | | | || (_| |\\ V  V / (_| |
+|_|  |_|\\___|_| |_|\\__\\__,_| \\_/\\_/ \\__,_|
+ `);
+}
+
+function menu() {
+  console.clear();
+  banner();
+
+  console.log("[1] Auto sender");
+  console.log("[2] Auto sender (save mode)");
+  console.log("[3] Tambah Nomor");
+  console.log("[4] Dapatkan Log Chat");
+}
+
+module.exports = { menu };
