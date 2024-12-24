823f1b1a228baa5dbe66e640e1b94d07d3d5937c
Akmal <akmalthemalong@gmail.com>
2024-12-24 18:29:44 +0800

    fix: chat log not showing

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -3,8 +3,9 @@ const logger = require("../logger/logger.js");
 function onMessageCreate(client) {
   logger.info("Membuat penanganan ketika client menerima pesan dari kontak");
 
-  client.on("message_crete", (message) => {
-    if (message.body.includes("@c.us")) {
+  client.on("message", (message) => {
+    // logger.info(message);
+    if (message.from.includes("@c.us")) {
       logger.info(`${message.from}: ${message.body}`);
     }
   });
