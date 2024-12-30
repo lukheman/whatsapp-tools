09c393b6e128248778ec0765711380d61802074e
Akmal <akmalthemalong@gmail.com>
2024-12-30 17:10:09 +0800

    update: app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
-const prompt = require("prompt-sync")();
-const config = require("./config/config_user.json");
-
 const components = require("./ui/components.js");
 
 const { isRegistered } = require("./utils/api.js");
+const prompt = require("prompt-sync")();
 
 const main = async () => {
+  components.banner();
   const status = await isRegistered();
 
   if (!status) {
+    prompt("[!] Lakukan registrasi terlebih dahulu, sebelum menggunakan tools");
     await components.registration();
+  } else {
+    await components.loginUsingToken();
   }
-  components.mainMenu();
 };
 
 main();
