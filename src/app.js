06a3a5d0383dc373272d7bb01ed547d417fb3af3
Akmal <akmalthemalong@gmail.com>
2025-01-02 15:51:36 +0800

    fix: no internet connection

--- src/app.js
+++ src/app.js
@@ -5,14 +5,21 @@ const prompt = require("prompt-sync")();
 
 const main = async () => {
   components.banner();
-  const status = await isRegistered();
-
-  if (!status) {
-    prompt("[!] Lakukan registrasi terlebih dahulu, sebelum menggunakan tools");
-    await components.registration();
-  } else {
-    console.log("[!] Anda telah dertaftar, silahkan login menggunakan cookie");
-    await components.loginUsingToken();
+  try {
+    const status = await isRegistered();
+    if (!status) {
+      prompt(
+        "[!] Lakukan registrasi terlebih dahulu, sebelum menggunakan tools",
+      );
+      await components.registration();
+    } else {
+      console.log(
+        "[!] Anda telah dertaftar, silahkan login menggunakan cookie",
+      );
+      await components.loginUsingToken();
+    }
+  } catch (error) {
+    console.log("[!] cek koneksi internet Anda");
   }
 };
 
